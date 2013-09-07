e6c9a7dc3b3a821cad839ed82743c98263a9b961
Oliver Manheim <omanheim@sas.upenn.edu>
2013-09-06 23:29:24 -0400

    wrote db models

--- awesome/settings.py
+++ awesome/settings.py
@@ -13,10 +13,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'pennapps',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
+        'USER': 'root',
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'courseAdvisor',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- courseAdvisor/models.py
+++ courseAdvisor/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Department(models.Model):
+	code = models.CharField(max_length = 5)
+	name = models.CharField(max_length = 30)
+	def __unicode__(self):
+		return name
+
+class Course(models.Model):
+	course_id = models.IntegerField()
+	title = models.CharField(max_length = 60)
+	description = models.CharField(max_length=1500,null=True)
+	keywords = models.ManyToManyField('Keyword')
+	departments = models.ManyToManyField('Department')
+	linkedCourses = models.ManyToManyField(
+		'self',
+		through='Links',
+		symmetrical=False,
+		related_name='linkedCourse+',
+	)
+	def __unicode__(self):
+		return title
+
+class Keyword(models.Model):
+	word = models.CharField(max_length = 30)
+	def __unicode__(self):
+		return word
+
+class Links(models.Model):
+	course1 = models.ForeignKey('Course', related_name='course1s')
+	course2 = models.ForeignKey('Course', related_name='course2s')
+	strength = models.IntegerField()
