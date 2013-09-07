4dfcbbd169743b2476d0391b859f13cac54cb64b
Oliver Manheim <omanheim@sas.upenn.edu>
2013-09-07 00:33:07 -0400

    model fix

--- courseAdvisor/models.py
+++ courseAdvisor/models.py
@@ -3,17 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Department(models.Model):
-	code = models.CharField(max_length = 5,unique=True)
+	code=models.CharField(max_length=5,unique=True)
 	def __unicode__(self):
 		return name
 
 class Course(models.Model):
-	course_id = models.IntegerField()
-	title = models.CharField(max_length = 60)
-	description = models.CharField(max_length=1500,null=True)
-	keywords = models.ManyToManyField('Keyword')
-	departments = models.ManyToManyField('Department')
-	linkedCourses = models.ManyToManyField(
+	courseCodes=models.CharField(max_length=1500)
+	title=models.CharField(max_length=60)
+	description=models.CharField(max_length=1500,null=True)
+	keywords=models.ManyToManyField('Keyword')
+	departments=models.ManyToManyField('Department')
+	linkedCourses=models.ManyToManyField(
 		'self',
 		through='Links',
 		symmetrical=False,
@@ -23,11 +23,11 @@ class Course(models.Model):
 		return title
 
 class Keyword(models.Model):
-	word = models.CharField(max_length = 30)
+	word=models.CharField(max_length=30)
 	def __unicode__(self):
 		return word
 
 class Links(models.Model):
-	course1 = models.ForeignKey('Course', related_name='course1s')
-	course2 = models.ForeignKey('Course', related_name='course2s')
-	strength = models.IntegerField()
+	course1=models.ForeignKey('Course', related_name='course1s')
+	course2=models.ForeignKey('Course', related_name='course2s')
+	strength=models.IntegerField()
