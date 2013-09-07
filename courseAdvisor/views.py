5f3ec4e79c9ba579adde0e59beb4f2c6dd5b3b04
Oliver Manheim <omanheim@sas.upenn.edu>
2013-09-07 04:29:17 -0400

    goin 2 sleep

--- courseAdvisor/views.py
+++ courseAdvisor/views.py
@@ -4,11 +4,7 @@ from django.shortcuts import render
 from courseAdvisor.models import Course
 
 def course_search(request):
-		if request.is_ajax():
-			message = request.POST
-		else:
-			message = 'nope'
-		return HttpResponse(message)
+	return HttpResponse('hi')
 
 def index(request):
-		return render(request, 'courseAdvisor/index.html')
+	return render(request, 'courseAdvisor/index.html')
