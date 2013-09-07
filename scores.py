252ac431022276fd17d9013bb68cb54cb9128a24
David Zbarsky <dzbarsky@gmail.com>
2013-09-07 03:59:30 -0400

    get keywords courses working

--- scores.py
+++ scores.py
@@ -6,24 +6,55 @@ import MySQLdb
 
 import pprint
 import nltk
+import string
 
 class DatabasePopulater:
     def __init__(self, key):
         self.key = key
+        self.db = MySQLdb.connect("localhost", "root", "", "PennApps" )
 
-    def create_tags(self, description):
-        text = nltk.word_tokenize(description)
-        #turns string output into a tuple in format (word, POS)
+    def executeQuery(self, sql):
+        cursor = self.db.cursor()
+        try:
+            cursor.execute(sql)
+            self.db.commit()
+            return True
+        except Exception as e:
+            print "\n~~~~~~~~FAILED: " + sql
+            print e
+            self.db.rollback()
+            return False
+
+    def create_tags(self, description, course_id):
+        # Strip punctuation
+        text = ''.join(ch for ch in description if ch not in string.punctuation)
+        text = nltk.word_tokenize(text)
         output_tuple = nltk.pos_tag(text);
-        self.find_nouns(output_tuple)
+        self.find_nouns(output_tuple, course_id)
 
-    def find_nouns(self,output_tuples):
+    def find_nouns(self, output_tuples, course_id):
+        occurrences = dict()
         for item in output_tuples:
-            if "NN" in item[1]:
-                print item[0]
-                #add keyword to the keyword list for the current course 
-                #add to keyword database (maps ints to keywords) 
+            if item[1] in ("NN", "NNP", "NNS"):
+                prev = 0
+                if item[0] in occurrences:
+                    prev = occurrences[item[0]]
+                occurrences[item[0]] = prev + 1
 
+                # Insert word into database
+                sql="""INSERT IGNORE INTO courseAdvisor_keyword(word)
+VALUES ('%s')""" % item[0]
+                self.executeQuery(sql)
+
+        for item in occurrences:
+            print item
+            print occurrences[item]
+            # Add keyword to the keyword list for the current course
+            sql="""INSERT INTO courseAdvisor_courses_keywords(course_id, number, keyword_id)
+SELECT %s, %s, id FROM courseAdvisor_keyword WHERE word='%s'""" % (course_id,
+                                                                   occurrences[item],
+                                                                   item)
+            self.executeQuery(sql)
 
     # Retrieves an endpoint on the Penn Course Review API
     def get_data(self, endpoint):
@@ -48,23 +79,8 @@ class DatabasePopulater:
         return course_ids
 
     def populate_database(self):
-
-        def execute(sql, db):
-            cursor = db.cursor()
-            try:
-                cursor.execute(sql)
-                db.commit()
-                return True
-            except Exception as e:
-                print "\n~~~~~~~~FAILED: " + sql
-                print e
-                db.rollback()
-                return False
-
         def escape(val):
             return val.replace("'", "\\'")
-
-        db = MySQLdb.connect("localhost", "root", "", "PennApps" )
 
         pp = pprint.PrettyPrinter(indent=4)
 
@@ -73,13 +89,12 @@ class DatabasePopulater:
             #pp.pprint(course)
 
             # Insert a course into the courses table
-            sql = """INSERT INTO courseAdvisor_course(id, courseCodes, title, description)
-VALUES (%s, '%s', '%s', '%s')""" % (course_id,
-                                    escape(json.dumps(course['result']['aliases'])),
-                                    escape(course['result']['name']),
-                                    escape(course['result']['description']))
-            if not execute(sql, db):
-                pp.pprint(course)
+            sql = """INSERT INTO courseAdvisor_course(id, courseCodes, title, description, preSearched)
+VALUES (%s, '%s', '%s', '%s', FALSE)""" % (course_id,
+                                           escape(json.dumps(course['result']['aliases'])),
+                                           escape(course['result']['name']),
+                                           escape(course['result']['description']))
+            self.executeQuery(sql)
 
             aliases = course['result']['aliases']
             for alias in aliases:
@@ -87,17 +102,17 @@ VALUES (%s, '%s', '%s', '%s')""" % (course_id,
                 dept = alias[0:alias.find('-')]
                 sql = """INSERT IGNORE INTO courseAdvisor_department(code)
 VALUES ('%s')""" % dept
-                execute(sql, db)
+                self.executeQuery(sql)
 
                 sql = """INSERT IGNORE INTO courseAdvisor_course_departments(course_id, department_id)
 SELECT %s, id FROM courseAdvisor_department WHERE code='%s'""" % (course_id,
                                                                   dept)
 
                 #print sql
-                execute(sql, db)
-            self.create_tags(course['result']['description'])
+                self.executeQuery(sql)
+            self.create_tags(course['result']['description'], course_id)
 
-        db.close()
+        self.db.close()
 
 def main(key):
     dp = DatabasePopulater(key)
