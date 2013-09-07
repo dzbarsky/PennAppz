e01bec1f0c30874128dff6d26e9847dba49acca5
David Zbarsky <dzbarsky@gmail.com>
2013-09-07 15:20:44 -0400

    Fixup view.py for new database

--- courseAdvisor/views.py
+++ courseAdvisor/views.py
@@ -2,7 +2,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from scores import Database
+from database import DatabaseManager
 from courseAdvisor.models import Course
 
 @csrf_exempt
@@ -11,7 +11,7 @@ def course_search(request):
 		print 'not ajax'
 		return
 	coursecode = request.POST['coursecode']
-	db = Database()
+	db = DatabaseManager()
 	courses = db.determine_searched_course(coursecode)
 	for course in courses:
 		print course
